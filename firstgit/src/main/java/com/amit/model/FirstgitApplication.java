eeed8687bea322586c7b220a85b71aceb40dbba1
amitpathade <amitpathadepatil@gmail.com>
2022-11-11 16:17:25 +0530

    FIR-1:create pakage amit.model

---
+++ firstgit/src/main/java/com/amit/model/FirstgitApplication.java
@@ -0,0 +1,13 @@
+package com.amit.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgitApplication.class, args);
+	}
+
+}
---
+++ firstgit/src/test/java/com/amit/model/FirstgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
